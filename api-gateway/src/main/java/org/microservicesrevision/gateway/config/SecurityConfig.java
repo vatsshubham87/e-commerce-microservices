aed12e866f2560ddb2dd32c40e1273cf2c63ab4d
shubham <vatsshubham87@gmail.com>
2025-10-02 20:13:54 +0530

    implemented centralized securtiy in api-gateway and removed from user-service

---
+++ api-gateway/src/main/java/org/microservicesrevision/gateway/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package org.microservicesrevision.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("admin")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // disable CSRF for APIs
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/actuator/**").permitAll() // actuator endpoints public
+                        .anyExchange().authenticated() // everything else secured
+                )
+                .httpBasic(httpBasic -> {}) // enable HTTP Basic Auth
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable); // disable login form
+        return http.build();
+    }
+}
+
+
